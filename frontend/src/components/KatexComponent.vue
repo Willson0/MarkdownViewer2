26d002ea279fefd0b542ab0210ddba38a1ff40f2
Willson0 <logvinovnikita3@gmail.com>
2025-11-30 23:06:21 +0500

    first

--- frontend/src/components/KatexComponent.vue
+++ frontend/src/components/KatexComponent.vue
@@ -120,17 +120,18 @@ export default {
                 } else return p;
             });
 
-            for (let part of this.parts) {
-                if (part.type === "text") this.fullText += "<span>" + part.content + "</span>"
-                else this.fullText += `<span data-tex="${part.content}" data-display="${part.display ? '1' : '0'}" class="${part.display ? 'display-math' : ''}">${part.html}</span>`;
-            }
-
             const md = new MarkdownIt({
                 html: true,
                 linkify: true,
                 breaks: true
             })
-            this.html = md.render(this.fullText);
+
+            for (let part of this.parts) {
+                if (part.type === "text") this.fullText += "<span>" + part.content + "</span>"
+                else this.fullText += `<span data-tex="${part.content}" data-display="${part.display ? '1' : '0'}" class="${part.display ? 'display-math' : ''}">${part.html}</span>`;
+            }
+
+            this.html = this.fullText;
 
             this.$nextTick(() => {
                 requestAnimationFrame(() => {
